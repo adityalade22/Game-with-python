854a9f25fed4d5dfa2583b3c6a28f0a7f3415bb8
adityalade22 <adityalade22@gmail.com>
2025-09-21 23:26:52 +0530

    fixed whole code

--- game.py
+++ game.py
@@ -1,16 +1,7 @@
-#This is my second comment where
-#I am going to write a comment
-#that spans multiple lines
-print("Hello, World!")
-#This is my third comment
-#This is my fourth comment
 A =int(input("Enter a number: "))
+
 if A<=9:
     print("the given number has only has one digits")
 
-#This is new feature
-elif A>=100:
-    print("the given number has three digits")
-    
 else:
     print("the given number has two digits")
